82b1b4752973c4d806487e1e43a6c02360b73505
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-15 23:42:11 +0100

    nix build with a fixed nixpkgs version

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import (import ./nixpkgs.nix) {}
 }:
 
 with pkgs;
---
+++ nixpkgs-version.nix
@@ -0,0 +1,7 @@
+# To update:
+# nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/07723f14bb132f6d65c74b953bef39cd80457273.tar.gz 
+
+{
+  rev    = "c7369800d11519d229f376c729957fb0c0a21782";
+  sha256 = "0a0lki60ivl0xw2y7b4q4acaj3xcdfisxq17mnwv6wmq9zxbx1bl";
+}
